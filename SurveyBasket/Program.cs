21e99f250f372e1e6c836eb91768c1cffd90b879
Mahmoud Khaled <abohallo112@gmail.com>
2026-02-13 15:11:22 +0200

    Added Logging Configurations

--- SurveyBasket/Program.cs
+++ SurveyBasket/Program.cs
@@ -1,4 +1,5 @@
 using FluentValidation.AspNetCore;
+using Serilog;
 using SurveyBasket;
 using SurveyBasket.Persistence;
 
@@ -7,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddIdentityApiEndpoints<ApplicationUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddDependencies(builder.Configuration);
+
+builder.Host.UseSerilog((context,configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
 
 
 var app = builder.Build();
@@ -17,7 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 
 app.UseCors();
